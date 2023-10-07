3edd0503004c1835c9de67ac3a41baf424f77807
Stelios Tzamarias <111519229+austrelios@users.noreply.github.com>
2023-10-07 20:23:39 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -23,14 +23,25 @@ var output_cvc = document.getElementById("cvc");
 const confirm_button = document.querySelector("#confirm_button");
 const continue_button = document.querySelector("#continue_button");
 
-function validate_Card_Details() {
+// Update the card details in realtime as each key is pressed
+input_holder_name.addEventListener("keyup", update_Card_Details);
+input_card_number.addEventListener("keyup", update_Card_Details);
+input_cvc.addEventListener("keyup", update_Card_Details);
+
+
+function initialise_Error_Messages() {
     error_holder_name.innerHTML = "";
     error_card_number.innerHTML = "";
     error_month_year.innerHTML = "";
     error_cvc.innerHTML = "";
+}
+
+function validate_Card_Details() {
 
     var card_details_valid = true;
     const regex = new RegExp("^[0-9]{13,19}$");
+
+    initialise_Error_Messages();
 
     if (!input_holder_name.value) {
         error_holder_name.innerHTML = "Name cannot be blank";
@@ -54,11 +65,15 @@ function validate_Card_Details() {
     }
     if (card_details_valid) {
         update_Card_Details();
+        show_Thank_You_Container();
     }
 }
 function update_Card_Details() {
+
+    initialise_Error_Messages();
+
     const unformatted_card_number = input_card_number.value.replace(" ", "");
-    let n = 5; // insert a character after every 5 characters
+    let n = 4; // insert a character after every 5 characters
     let padding_character = " ";
     let formatted_card_number = "";
     for (let i = 0; i < unformatted_card_number.length; i += n) {
@@ -71,9 +86,10 @@ function update_Card_Details() {
 
     output_holder_name.innerHTML = input_holder_name.value;
     output_card_number.innerHTML = formatted_card_number;
+
     output_cvc.innerHTML = input_cvc.value;
 
-    show_Thank_You_Container();
+
 }
 
 function show_Thank_You_Container() {
